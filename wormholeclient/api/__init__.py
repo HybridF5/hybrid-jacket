fb267673077c0ae17171111fc7ac27fb86d6b74c
nkapotoxin <nkapotoxin@gmail.com>
2016-03-22 10:58:32 +0800

    change name to volumes.py

--- wormholeclient/api/__init__.py
+++ wormholeclient/api/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from .container import ContainerApiMixin
-from .volume import VolumeApiMixin
+from .volumes import VolumeApiMixin
 from .network import NetworkApiMixin
 from .personality import PersonalityApiMixin
 from .version import VersionApiMixin
