9606b4f1ba3575f5bb94b57b207d11218553123b
liulinghui <llhuii5614@gmail.com>
2016-03-23 11:15:53 +0800

    Delete volume.py

--- wormholeclient/api/volumes.py
+++ wormholeclient/api/volumes.py
@@ -17,3 +17,10 @@ class VolumeApiMixin(object):
         params = {'t': timeout}
         url = self._url("/volumes")
         return self._result(self._get(url, params=params), True)
+
+    def clone_volume(self, volume, src_vref, timeout=10):
+        params = {'t': timeout}
+        url = self._url("/volumes/clone")
+        res = self._post_json(url, data={ 'volume' : { 'id' : volume['id'], 'size' : volume['size']}, 
+            'src_vref': { 'id': src_vref['id'], 'size': src_vref['size'] } })
+        return self._result(res, True)
